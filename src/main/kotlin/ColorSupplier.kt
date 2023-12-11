a36290c61ca07750ffd7afca26d8a59c8c94095b
boroda4436 <chupika.bohdan@ukr.net>
2023-12-11 15:42:19 +0200

    initial commit

---
+++ src/main/kotlin/ColorSupplier.kt
@@ -0,0 +1,5 @@
+class ColorSupplier {
+    fun getRandomColor(): String {
+        return "";
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+    // create three balls using class Lottery and print information about them in console
+}
